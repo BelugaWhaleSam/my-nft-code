4cbcb2d542edaffe35ee985ad2dff6897b25732a
BelugaSam <sameermohd2k1@gmail.com>
2022-11-01 11:28:18 +0530

    gitignore cached

--- Client/MyNft/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-// Smart contract ABI
-import abi from './MyNFT.json';
-export const contractABI = abi.abi;
-
-// Contract address generated on deployment
-export const contractAddress = '0x8a0b8c2E9fd2FB3AdA6ce46f4f47eE45c00b814C';
